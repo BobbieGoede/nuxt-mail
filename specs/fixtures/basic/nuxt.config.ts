3c29e2d391d6a68da278358518f5e1d881c2a4f8
Bobbie Goede <bobbiegoede@gmail.com>
2024-08-30 13:45:17 +0200

    ci: fix fixture type stub

--- specs/fixtures/basic/nuxt.config.ts
+++ specs/fixtures/basic/nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
       { name: "cc", cc: "johndoe@gmail.com" },
       { name: "cc-bcc", cc: "cc@gmail.com", bcc: "bcc@gmail.com" },
     ],
+    smtp: {},
   },
 
   compatibilityDate: "2024-08-29",
